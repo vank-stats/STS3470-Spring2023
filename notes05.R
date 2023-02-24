693a2b1fd15b58ff23d97a6b59821df795073a9a
Ryne VanKrevelen <rvankrevelen@elon.edu>
2023-02-24 10:03:33 -0500

    Updated notes 5 code and comments

--- notes05.R
+++ notes05.R
@@ -2,7 +2,7 @@
 
 # Labeling graphs
 
-# Example from notes
+# Example from notes illustrating labs() function
 
 library(ggplot2)
 ggplot(diamonds, aes(x = carat, y = price)) +
@@ -15,6 +15,8 @@ ggplot(diamonds, aes(x = carat, y = price)) +
        shape = "Diamond cut")
 
 
+# Example of default legend label if we don't include shape in labs()
+
 ggplot(diamonds, aes(x = carat, y = price)) +
   geom_point(aes(shape = cut)) +
   labs(title = "How diamond prices relate to carat",
@@ -22,6 +24,9 @@ ggplot(diamonds, aes(x = carat, y = price)) +
        caption = "Created for STS3470 Notes 05",
        x = "Carat (One carat = 1/5 of a gram)",
        y = "Price (in dollars)")
+
+
+# Example of adjusting labels if we use color instead of shape
 
 ggplot(diamonds, aes(x = carat, y = price)) +
   geom_point(aes(color = cut)) +
@@ -34,16 +39,49 @@ ggplot(diamonds, aes(x = carat, y = price)) +
 
 
 
-# What happens when a constant color is put inside aes()?
+
+# Changing point color
+
+# Assign specific color outside aes() to change all points one color
+
+ggplot(Orange, aes(x = age, y = circumference)) + 
+  geom_point(color = "violet")
+
+
+# Put color argument inside aes() to map a variable to the color
+
+ggplot(Orange, aes(x = age, y = circumference)) + 
+  geom_point(aes(color = Tree))
+
+
+# You can change your legend order - two options
+
+# Option 1
+
+ggplot(Orange, aes(x = age, y = circumference)) + 
+  geom_point(aes(color = Tree)) +
+  scale_color_discrete(limits = c("1", "2", "3", "4", "5"))
+
+# Option 2
+
+ggplot(Orange, aes(x = age, y = circumference)) + 
+  geom_point(aes(color = factor(Tree, levels = 1:5)))
+
+
+
+# Question: What happens when a constant color is put inside aes()?
 
 ggplot(Orange, aes(x = age, y = circumference)) + 
   geom_point(aes(color = "violet"))
 
 
-# What happens when we map a variable to color without aes()?
+# Question: What happens when we map a variable to color without aes()?
 
 ggplot(Orange, aes(x = age, y = circumference)) + 
   geom_point(color = Tree)
+
+
+# If there was an object called Tree, it could use that to show the color
 
 Tree <- "blue"
 ggplot(Orange, aes(x = age, y = circumference)) + 
@@ -66,6 +104,8 @@ ggplot(mtcars, aes(x = wt, y = mpg)) +
        color = "Cylinders") +
   theme_bw()
 
+
+# Question about how to manually choose the colors - scale_color_manual()
 
 ggplot(mtcars, aes(x = wt, y = mpg, shape = factor(cyl), color = factor(cyl))) +
   geom_point(size = 6, alpha = 0.5) +
